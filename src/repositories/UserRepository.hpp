b18fc8725add4c34acda02a2a9475cd8142fefb5
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-20 20:42:02 +0530

    feat: integrate src/repositories/UserRepository.hpp

---
+++ src/repositories/UserRepository.hpp
@@ -0,0 +1,11 @@
+#ifndef USERREPOSITORY_HPP
+#define USERREPOSITORY_HPP
+
+#include "src/models/User.hpp"
+
+class UserRepository {
+public:
+    void getUser();
+};
+
+#endif  // USERREPOSITORY_HPP
