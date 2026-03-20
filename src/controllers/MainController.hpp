bd0588de7766a73334d182d4fdf70ad9d2990b90
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-20 20:41:54 +0530

    feat: integrate src/controllers/MainController.hpp

---
+++ src/controllers/MainController.hpp
@@ -0,0 +1,11 @@
+#ifndef MAINCONTROLLER_HPP
+#define MAINCONTROLLER_HPP
+
+#include "src/services/UsersService.hpp"
+
+class MainController {
+public:
+    void run();
+};
+
+#endif  // MAINCONTROLLER_HPP
