30a2337601e0fae1f888755f851bf23ce6f851d0
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-20 20:41:50 +0530

    feat: integrate src/config/config.hpp

---
+++ src/config/config.hpp
@@ -0,0 +1,12 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#include <string>
+
+class Config {
+public:
+    static const std::string DB_HOST;
+    static const std::string DB_PORT;
+};
+
+#endif  // CONFIG_HPP
